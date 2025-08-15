fdba818a9d161c0b00aaa8f528117d07edcb9c34
Elchrist01 <crismonter_daniel@hotmail.com>
2025-08-15 00:32:04 -0500

    creando la pagina de arquitectura

---
+++ JAVA/Arqui.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+// Función para generar un color aleatorio en formato hexadecimal
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+// Aplicar el color aleatorio al fondo del body
+document.body.style.backgroundColor = getRandomColor();
+
+
